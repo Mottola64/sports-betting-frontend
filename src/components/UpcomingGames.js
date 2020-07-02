b18dd58412cc75ccc76cf347606db4b2ee5fe231
Michael Mottola <mottola64@gmail.com>
2020-07-02 13:02:53 -0400

    fetch working correctly

--- src/components/UpcomingGames.js
+++ src/components/UpcomingGames.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import UpcomingGame from './UpcomingGame'
 
 class UpcomingGames extends React.Component {
     render() {
         return(
-        <div>{this.props.upcoming}</div>
+            <div>
+            {this.props.upcoming.map((upcomingGame) => (
+              <UpcomingGame key={upcomingGame.id} upcomingGame={upcomingGame} />
+            ))}
+          </div>
         )
     }
 }
