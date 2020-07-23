4cd1a831a25cb4c655aa02ed1068486fcdb7b26c
Michael Mottola <mottola64@gmail.com>
2020-07-23 14:05:13 -0400

    team pictures showing on scheduled games

--- src/components/DailySchedule.js
+++ src/components/DailySchedule.js
@@ -2,36 +2,69 @@ import React from "react";
 import {
   Card,
   CardHeader,
+  CardImg,
   CardColumns,
   CardSubtitle,
   CardBody,
 } from "reactstrap";
+import teamPictures from "../data/pictures";
+
 class DailySchedule extends React.Component {
-render() {
-    return(
-        <div>
-          <h1>MLB Games Today</h1>
-          <CardColumns>
-        {this.props.dailyschedule
-        .filter(dailyScheduledGame => dailyScheduledGame.status.short !== "CANC").map((dailyScheduledGame) => {
-          return (
-            <Card key={dailyScheduledGame.id}>
-              <CardBody>
-                <CardHeader className="text-center" width="100%">
-                  <strong>{dailyScheduledGame.teams.away.name} at {dailyScheduledGame.teams.home.name}</strong>
-                </CardHeader>
-                <br></br>
-                <CardSubtitle>
-                  
-                </CardSubtitle>
-              </CardBody>
-            </Card>
-          );
-        })}
-      </CardColumns>
+  
+  
+  render() {
+    return (
+      <div>
+        <h1>MLB Games Today</h1>
+        <CardColumns>
+          {this.props.dailyschedule
+            .filter(
+              (game) =>
+                game.status.short !== "CANC"
+            )
+            .filter(
+              (game) =>
+                game.status.short !== "POST"
+            )
+            .map((dailyScheduledGame) => {
+              return (
+                <Card key={dailyScheduledGame.id}>
+                  <CardBody>
+                    <CardHeader className="text-center" width="100%">
+                      <strong>
+                        {dailyScheduledGame.teams.away.name}
+                        at {dailyScheduledGame.teams.home.name}
+                      </strong>
+                    </CardHeader>
+                    <br></br>
+                    <CardSubtitle><CardImg
+                          top src={teamPictures[dailyScheduledGame.teams.away.name]}
+                          style=
+                          {{
+                            width: "40%",
+                            height: "40%",
+                            backgroundColor: "white",
+                          }}
+                          alt="Card image cap" />{" "}
+                        at
+                        <CardImg
+                          top
+                          src={teamPictures[dailyScheduledGame.teams.home.name]}
+                          style={{
+                            width: "40%",
+                            height: "40%",
+                            backgroundColor: "white",
+                          }}
+                          alt="Card image cap"
+                        /></CardSubtitle>
+                  </CardBody>
+                </Card>
+              );
+            })}
+        </CardColumns>
       </div>
-    )
-}
+    );
+  }
 }
 
-export default DailySchedule
+export default DailySchedule;
---
+++ src/data/pictures.js
@@ -0,0 +1,65 @@
+import angels from "../assets/images/angels.svg";
+import diamondbacks from "../assets/images/diamondbacks.svg";
+import braves from "../assets/images/braves.svg";
+import orioles from "../assets/images/orioles.svg";
+import redsox from "../assets/images/red-sox.svg";
+import cubs from "../assets/images/cubs.svg";
+import whitesox from "../assets/images/white-sox.svg";
+import reds from "../assets/images/reds.svg";
+import indians from "../assets/images/indians.svg";
+import rockies from "../assets/images/rockies.svg";
+import tigers from "../assets/images/tigers.svg";
+import marlins from "../assets/images/marlins.svg";
+import astros from "../assets/images/astros.svg";
+import royals from "../assets/images/royals.svg";
+import dodgers from "../assets/images/dodgers.svg";
+import brewers from "../assets/images/brewers.svg";
+import twins from "../assets/images/twins.svg";
+import nationals from "../assets/images/nationals.svg";
+import mets from "../assets/images/mets.svg";
+import yankees from "../assets/images/yankees.svg";
+import athletics from "../assets/images/athletics.svg";
+import phillies from "../assets/images/phillies.svg";
+import pirates from "../assets/images/pirates.svg";
+import padres from "../assets/images/padres.svg";
+import giants from "../assets/images/giants.svg";
+import mariners from "../assets/images/mariners.svg";
+import cardinals from "../assets/images/cardinals.svg";
+import devilrays from "../assets/images/devil-rays.svg";
+import rangers from "../assets/images/rangers.svg";
+import bluejays from "../assets/images/blue-jays.svg";
+
+const teamPictures = {
+  "Los Angeles Angels": angels,
+  "Arizona Diamondbacks": diamondbacks,
+  "Atlanta Braves": braves,
+  "Baltimore Orioles": orioles,
+  "Boston Red Sox": redsox,
+  "Chicago Cubs": cubs,
+  "Chicago White Sox": whitesox,
+  "Cincinnati Reds": reds,
+  "Cleveland Indians": indians,
+  "Colorado Rockies": rockies,
+  "Detroit Tigers": tigers,
+  "Miami Marlins": marlins,
+  "Houston Astros": astros,
+  "Kansas City Royals": royals,
+  "Los Angeles Dodgers": dodgers,
+  "Milwaukee Brewers": brewers,
+  "Minnesota Twins": twins,
+  "Washington Nationals": nationals,
+  "New York Mets": mets,
+  "New York Yankees": yankees,
+  "Oakland Athletics": athletics,
+  "Philadelphia Phillies": phillies,
+  "Pittsburgh Pirates": pirates,
+  "San Francisco Giants": giants,
+  "Seattle Mariners": mariners,
+  "St Louis Cardinals": cardinals,
+  "Tampa Bay Rays": devilrays,
+  "Texas Rangers": rangers,
+  "Blue Jays": bluejays,
+  "San Diego Padres": padres,
+};
+
+export default teamPictures;
