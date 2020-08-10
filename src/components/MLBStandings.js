f872cfb78a18da124aeef0b948cf0a230a80398e
Michael Mottola <mottola64@gmail.com>
2020-08-09 23:48:45 -0400

    using axios, beginning to build out a table with react-table

--- src/components/MLBStandings.js
+++ src/components/MLBStandings.js
@@ -1,11 +1,35 @@
 import React from "react";
+import axios from 'axios'
+import Table from './Table'
 
-class MLBStandings extends React.Component {
-    render(){
-        return(
-            <div>MLBStandings</div>
-        )
-    }
+function MLBStandings() {
+    let [responseData, setResponseData] = React.useState('');
+
+    const fetchData = React.useCallback(() => {
+        axios({
+          "method": "GET",
+          "url": "https://api-baseball.p.rapidapi.com/standings?league=1&season=2020",
+          "headers": {
+            "content-type": "application/json",
+            "x-rapidapi-host": "api-baseball.p.rapidapi.com",
+          "x-rapidapi-key": `${process.env.REACT_APP_DAILYSCHEDULE_API_KEY}`
+          }
+        })
+        .then((response) => {
+          setResponseData(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      }, [])
+      React.useEffect(() => {
+        fetchData()
+      }, [fetchData])
+  return (
+    <div className="App">
+        
+    </div>
+  );
 }
 
 export default MLBStandings;
