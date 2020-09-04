df2285fb9426c134b565be03f62743b465e0b70f
Michael Mottola <mottola64@gmail.com>
2020-09-04 15:28:45 -0400

    working on nba cards

--- src/components/NbaResults.js
+++ src/components/NbaResults.js
@@ -11,14 +11,18 @@ import {
 import nbaLogos from '../data/nbaLogos'
 
 class NbaResults extends React.Component {
+  
   render() {
+    function getEmbedString(str) {
+      return str.split('=')[1];
+  }
     return (
       <div>
         <h1>NBA Results</h1>
         <CardColumns>
           {this.props.nbaresults.map((nbaResult) => {
             return (
-              <Card key={nbaResult.idEvent}>
+              <Card style={{width: "420px", height: "375px"}} key={nbaResult.idEvent}>
                 <CardBody>
                   <CardHeader className="text-center" width="100%" margin="10px">
                     <strong>{nbaResult.strEventAlternate}</strong>
@@ -41,9 +45,11 @@ class NbaResults extends React.Component {
                       }}
                       alt="Card image cap"
                     /><br></br>
+                    <strong>{nbaResult.dateEventLocal}</strong><br></br>
                       <strong>{nbaResult.strAwayTeam}</strong>: {nbaResult.intAwayScore}<br></br>
                       <strong>{nbaResult.strHomeTeam}</strong>: {nbaResult.intHomeScore}<br></br>
                   </CardSubtitle>
+                  {/* <CardSubtitle style={{marginTop: "10px"}}><iframe style={{maxWidth: '640px', maxHeight: '360px', margin: '0px'}} className="embed-responsive-item" title="videoplayer" src={`https://www.youtube.com/embed/${nbaResult.strVideo.split('=')[1]}`} gesture="media" allow="encrypted-media" allowFullScreen/></CardSubtitle>  */}
                   <Button
                     style={{padding: "10px", margin: "10px"}}
                     variant="primary"
--- src/components/NbaSchedule.js
+++ src/components/NbaSchedule.js
@@ -8,7 +8,7 @@ import {
   CardBody,
 } from "reactstrap";
 import Moment from "react-moment";
-import nbaLogos from '../data/nbaLogos'
+import nbaLogos from "../data/nbaLogos";
 
 class NbaSchedule extends React.Component {
   render() {
@@ -16,42 +16,51 @@ class NbaSchedule extends React.Component {
       <div>
         <h1>Upcoming NBA Games</h1>
         <CardColumns>
-          {this.props.nbaschedule.map((nbaScheduledGame) => {
-            return (
-              <Card id="nbacard" key={nbaScheduledGame.idEvent}>
-                <CardBody>
-                  <CardHeader className="text-center" width="100%">
-                    <strong>{nbaScheduledGame.strEventAlternate}</strong>
-                  </CardHeader>
-                  <br></br>
-                  <CardSubtitle>
-                    {nbaScheduledGame.strTime > Date() && <Moment parse="HH:mm:ss">
+          {this.props.nbaschedule
+            .filter((game) => game.intAwayScore === null)
+            .map((nbaScheduledGame) => {
+              return (
+                <Card
+                  style={{ width: "420px", height: "330px" }}
+                  id="nbacard"
+                  key={nbaScheduledGame.idEvent}
+                >
+                  <CardBody>
+                    <CardHeader className="text-center" width="100%">
+                      <strong>{nbaScheduledGame.strEventAlternate}</strong>
+                    </CardHeader>
+                    <br></br>
+                    <CardSubtitle>
+                      {/* {nbaScheduledGame.strTime < Date() && <Moment parse="HH:mm:ss">
                       <strong>{nbaScheduledGame.strTime}</strong>
-            </Moment>}
-                    
-                  </CardSubtitle><br></br>
-                  <CardSubtitle>
-                  <CardImg
-                      src={nbaLogos[nbaScheduledGame.strAwayTeam]}
-                      style={{
-                        width: "30%",
-                            height: "30%",
-                      }}
-                      alt="Card image cap"
-                    />{" "}
-                    <CardImg
-                      src={nbaLogos[nbaScheduledGame.strHomeTeam]}
-                      style={{
-                        width: "30%",
-                        height: "30%",
-                      }}
-                      alt="Card image cap"
-                    />
-                  </CardSubtitle>
-                </CardBody>
-              </Card>
-            );
-          })}
+            </Moment>} */}
+                      {/* <Moment format = "YYYY-MM-DD">
+                        {nbaScheduledGame.dateEvent}
+                      </Moment> */}
+                    </CardSubtitle>
+                    <br></br>
+                    <CardSubtitle>
+                      <CardImg
+                        src={nbaLogos[nbaScheduledGame.strAwayTeam]}
+                        style={{
+                          width: "30%",
+                          height: "30%",
+                        }}
+                        alt="Card image cap"
+                      />{" "}
+                      <CardImg
+                        src={nbaLogos[nbaScheduledGame.strHomeTeam]}
+                        style={{
+                          width: "30%",
+                          height: "30%",
+                        }}
+                        alt="Card image cap"
+                      />
+                    </CardSubtitle>
+                  </CardBody>
+                </Card>
+              );
+            })}
         </CardColumns>
       </div>
     );
