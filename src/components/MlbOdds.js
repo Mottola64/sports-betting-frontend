4761b187d1113f9b84b7d7f66334a8b3a6682163
Michael Mottola <mottola64@gmail.com>
2020-10-05 18:51:11 -0400

    toggle button on odds

--- src/components/MlbOdds.js
+++ src/components/MlbOdds.js
@@ -1,61 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import {
+  Button,
   Card,
   CardHeader,
   CardImg,
   CardColumns,
   CardSubtitle,
   CardBody,
+  UncontrolledCollapse,
 } from "reactstrap";
 import Moment from "react-moment";
 import teamPictures from "../data/pictures";
 
-class MlbOdds extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>MLB Odds</h1>
-        <CardColumns>
-          {this.props.mlbodds.map((mlbGame) => {
-            return (
-              <Card key={mlbGame.teams[0]}>
-                <CardBody>
-                  <CardHeader className="text-center" width="100%">
-                    <strong>{mlbGame.teams[0]}</strong>
-                  </CardHeader>
-                  <br></br>
-                  <CardSubtitle>
-                    <Moment unix format="LT">
-                      {mlbGame.commence_time}
-                    </Moment>
-                  </CardSubtitle>
-                  <br></br>
-                  <CardSubtitle>
-                    <CardImg
-                      src={teamPictures[mlbGame.teams[1]]}
-                      style={{
-                        width: "30%",
-                        height: "30%",
-                      }}
-                      alt="Card image cap"
-                    />{" "}
-                    <CardImg
-                      src={teamPictures[mlbGame.teams[0]]}
-                      style={{
-                        width: "30%",
-                        height: "30%",
-                      }}
-                      alt="Card image cap"
-                    />
-                  </CardSubtitle>
-                </CardBody>
-              </Card>
-            );
-          })}
-        </CardColumns>
-      </div>
-    );
-  }
-}
+const MlbOdds = (props) => {
+  
+
+  return (
+    <div>
+      <h1>MLB Odds</h1>
+      <CardColumns>
+        {props.mlbodds.map((mlbGame) => {
+          return (
+            <Card key={mlbGame.teams[0]}>
+              <CardBody>
+                <CardHeader className="text-center" width="100%">
+                  <strong>
+                    {mlbGame.teams[0]} @ {mlbGame.teams[1]}
+                  </strong>
+                </CardHeader>
+                <br></br>
+                <CardSubtitle>
+                  <Moment unix format="LT">
+                    {mlbGame.commence_time}
+                  </Moment>
+                </CardSubtitle>
+                <br></br>
+                <CardSubtitle>
+                  <CardImg
+                    src={teamPictures[mlbGame.teams[0]]}
+                    style={{
+                      width: "30%",
+                      height: "30%",
+                    }}
+                    alt="Card image cap"
+                  />{" "}
+                  <CardImg
+                    src={teamPictures[mlbGame.teams[1]]}
+                    style={{
+                      width: "30%",
+                      height: "30%",
+                    }}
+                    alt="Card image cap"
+                  />
+                </CardSubtitle>
+                <Button id="toggler">Odds</Button>
+                <UncontrolledCollapse toggler={"toggler"}>
+                  {mlbGame.sites.map((site) => {
+                    return (
+                      <ul key={site.site_nice}>
+                        {site.site_nice}
+                        <li>
+                          {mlbGame.teams[0]}: {site.odds.h2h[1]}
+                        </li>
+                        <li>
+                          {mlbGame.teams[1]}: {site.odds.h2h[0]}
+                        </li>
+                      </ul>
+                    );
+                  })}
+                </UncontrolledCollapse>
+              </CardBody>
+            </Card>
+          );
+        })}
+      </CardColumns>
+    </div>
+  );
+};
 
 export default MlbOdds;
